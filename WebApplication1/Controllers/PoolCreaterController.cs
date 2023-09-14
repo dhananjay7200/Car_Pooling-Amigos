fffa17a8abc160f15b9180c9af723393855bfc28
Dhananjay Sudhakar Khuje <dhananjaykh@cybage.com>
2023-09-14 15:13:33 +0530

    controller added

---
+++ WebApplication1/Controllers/PoolCreaterController.cs
@@ -0,0 +1,57 @@
+﻿using Car_pooling.Interfaces;
+using Car_pooling.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Car_pooling.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PoolCreaterController : ControllerBase
+    {
+        private IPoolCreater _poolCreater;
+
+        public PoolCreaterController(IPoolCreater poolCreater)
+        {
+            _poolCreater = poolCreater;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetPoolCreater(int id)
+        {
+            IEnumerable<PoolingCreater> data = await _poolCreater.getPoolDetails(id);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return Ok(data);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddPool(PoolingCreater pool)
+        {
+            Task<string> data=_poolCreater.CreatePool(pool);
+            if (data == null)
+            {
+                return BadRequest();
+            }
+            return Ok(data);    
+        }
+
+        [HttpGet("/cities")]
+        public async Task<IEnumerable<PoolingCreater>> GetRidecities()
+        {
+
+            IEnumerable<PoolingCreater> cityList= await _poolCreater.GetRideStartingcities();
+            return cityList;
+
+        }
+
+        [HttpPost("/getride")]
+        public async Task<IEnumerable<PoolingCreater>> GetRide(PoolingCreater pool)
+        {
+            IEnumerable<PoolingCreater> poolList=await _poolCreater.GetRidesByCities(pool);
+            return poolList;
+        }
+    }
+}
---
+++ WebApplication1/Controllers/UserDetailsController.cs
@@ -0,0 +1,46 @@
+﻿using Car_pooling.Interfaces;
+using Car_pooling.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Car_pooling.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserDetailsController : ControllerBase
+    {
+        private IUserDetails _IUser;
+
+        public UserDetailsController(IUserDetails iUser)
+        {
+            _IUser = iUser;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UserLoginAsync(LoginDetails user)
+        {
+            string data = await _IUser.UserLogin(user);
+            if(data.Equals("not found"))
+            {
+                return NotFound();
+            }
+            return Ok(new Tokens { Token = data });
+
+        }
+
+        [HttpPost("/register")]
+        public async Task<ActionResult> RegisterUser(UserDetail user) {
+            UserDetail data=await _IUser.UserRegister(user);
+            if (data == null)
+            {
+                return BadRequest();
+            }
+            return Ok(data);
+        }
+
+
+    }
+}
